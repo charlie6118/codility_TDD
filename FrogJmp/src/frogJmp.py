74843ae3fe28a2e5d988658241454adc5111a3db
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-03-27 11:41:35 +0800

    Lesson 3 frogJmp

--- FrogJmp/src/frogJmp.py
+++ FrogJmp/src/frogJmp.py
@@ -1,2 +1,7 @@
-def solution():
-    pass
+def solution(X, Y, D):
+    if X == Y:
+        return 0
+    result = abs(Y-X) / D
+    if result > round(result):
+        return round(result) + 1
+    return round(result)
--- FrogJmp/test/test_frogJmp.py
+++ FrogJmp/test/test_frogJmp.py
@@ -1,2 +1,5 @@
 import pytest
 from src.frogJmp import solution
+
+def test_example1():
+    assert solution(85, 10, 30) == 3
