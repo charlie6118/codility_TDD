ff892c534689aa527987f1df3d93650cddf822a5
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-03-31 21:30:31 +0800

    lesson 4 frogRiverOne solution from Code Says

--- FrogRiverOne/src/frogRiverOne.py
+++ FrogRiverOne/src/frogRiverOne.py
@@ -1,16 +1,28 @@
 import array as arr
 
 def solution(X, A):
-
-    allNeedPath = list(range(1, X + 1))
-
+    allNeedPath = [-1] * X
+    uncovered = X
+    
     for index, value in enumerate(A):
-        try:
-            allNeedPath.remove(value)
-        except:
-            pass
-        if len(allNeedPath) == 0:
-            return index
+        if allNeedPath[value - 1] == -1:
+            allNeedPath[value - 1] = 1
+            uncovered -= 1
+        if uncovered == 0:
+            return index 
     return -1
+
+
+    #performance 1/5
+    # allNeedPath = list(range(1, X + 1))
+
+    # for index, value in enumerate(A):
+    #     try:
+    #         allNeedPath.remove(value)
+    #     except:
+    #         pass
+    #     if len(allNeedPath) == 0:
+    #         return index
+    # return -1
 
 print(solution(5, arr.array('b', [1, 3, 1, 4, 2, 3, 5, 4])))
