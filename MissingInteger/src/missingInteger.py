a9681ed0dcb8c812b9226815376e4bceca9acb4e
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-04-01 21:13:04 +0800

    lesson 4 MissingInteger -one more space

--- MissingInteger/src/missingInteger.py
+++ MissingInteger/src/missingInteger.py
@@ -1,7 +1,7 @@
 from array import array
 
 def solution(A):
-    space = [0] * len(A)
+    space = [0] * (len(A) + 1)
     
     for value in A:
         if 0 < value <= len(A):
@@ -10,7 +10,5 @@ def solution(A):
     for index, value in enumerate(space):
         if value == 0:
             return index + 1
-            
-    return len(A) + 1
 
 print(solution(array('b', [1, 2, 3])))
