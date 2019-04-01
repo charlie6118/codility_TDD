90558814d308d1270dae7e4a87410206ec88e51a
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-04-01 20:32:05 +0800

    lesson 4 maxCounter solution from stackoverflow

--- MaxCounters/src/maxCounters.py
+++ MaxCounters/src/maxCounters.py
@@ -1,19 +1,39 @@
 import array as arr
 
 def solution(N, A):
+    # credit. Andrei Simionescu 
+
     counters = [0] * N
-    def increase(counters, X):
-        counters[X] += 1
-        return counters
-    def maxCounters(counters):
-        maxCounter = max(counters)
-        return [maxCounter] * N
+    finalMax = 0
+    maxAfterOper = 0
 
     for _, value in enumerate(A):
-        if 1 <= value <= N:
-            counters = increase(counters, value - 1)
-        if value == N + 1:
-            counters = maxCounters(counters)
+        value -= 1
+        if value == N:
+            finalMax = maxAfterOper
+            continue 
+        if 0 <= value < N:
+            counters[value] = max(counters[value], finalMax) + 1
+            maxAfterOper = max(counters[value], maxAfterOper)
+
+    for index in range(N):
+        counters[index] = max(counters[index], finalMax)
+
     return counters
-        
+
+    # counters = [0] * N
+    # def increase(counters, X):
+    #     counters[X] += 1
+    #     return counters
+    # def maxCounters(counters):
+    #     maxCounter = max(counters)
+    #     return [maxCounter] * N
+
+    # for _, value in enumerate(A):
+    #     if 1 <= value <= N:
+    #         counters = increase(counters, value - 1)
+    #     if value == N + 1:
+    #         counters = maxCounters(counters)
+    # return counters
+
 print(solution(5, arr.array('b', [3, 4, 4, 6, 1, 4, 4])))
