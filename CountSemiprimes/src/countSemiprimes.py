2baef9e99d6f8ac3f2b95c8c56ddeeabc7e34581
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-04-14 22:48:05 +0800

    CountSemiprimes -Use factorization to determine the semiprimes

--- CountSemiprimes/src/countSemiprimes.py
+++ CountSemiprimes/src/countSemiprimes.py
@@ -11,75 +11,44 @@ def sieve(n):
                 k += i
         i += 1
     return sieve
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
 
-def sieveNums(n):
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
+def arrayF(N):
+    F = [0] * (N + 1)
     i = 2
-    while i * i <= n:
-        if sieve[i]:
-            k = i * i
-            print(k)
-            while(k <= n):
-                sieve[k] = False
-                k += i
-        i += 1
-    sieve_nums = []
-    for index in range(len(sieve)):
-        if sieve[index] == True:
-            sieve_nums.append(index)
-    return sieve_nums
-
-def getRange(P, Q):
-    range_pair = []
-    for index in range(len(P)):
-        range_pair.append((P[index], Q[index]))
-    return (range_pair)
-
-def getSemiprimes(N, P, Q):
-    sieve_nums = sieveNums(max(Q))
-    semiprimes = set()
-    for i in range(len(sieve_nums)):
-        for j in range(i, len(sieve_nums)):
-            if sieve_nums[i] * sieve_nums[j] > N:
-                break
-            semiprimes.add(sieve_nums[i] * sieve_nums[j])
-    return list(semiprimes)
-
-def solution(N, P, Q):
-    range_pair = getRange(P, Q)
-    semiprimes = getSemiprimes(N, P, Q)
-    result = []
-    for (p, q) in range_pair:
-        count = 0
-        for semiprime in semiprimes:
-            if p <= semiprime <= q:
-                count += 1
-        result.append(count)
-        count == 0
-    return result
-
-def arrayF(n):
-    F = [0] * (n + 1)
-    i = 2
-    while i * i <= n:
+    while i * i <= N:
         if F[i] == 0:
             k = i * i
-            while k <= n:
+            while k <= N:
                 if F[k] == 0:
                     F[k] = i
                 k += i
         i += 1
     return F
 
+def solution(N, P, Q):
+    # write your code in Python 3.6
+    f = arrayF(N)
+    prefixSum = [0] * (N + 1)
+    for i in range(1, N + 1):
+        prefixSum[i] = prefixSum[i - 1]
+        if f[i] != 0:
+            first_factor = f[i]
+            secont_factor = int(i / first_factor)
+            if f[first_factor] == 0 and f[secont_factor] == 0:
+                prefixSum[i] += 1
+    result = []
+    for i in range(len(Q)):
+        result.append(prefixSum[Q[i]] - prefixSum[P[i] - 1])
+    return result
+
 def factorization(x, F):
     primeFactors = []
-    while F[x] > 0:
+    while (F[x] > 0):
+        print(x, F[x])
         primeFactors += [F[x]]
-        x /= F[x]
+        print(primeFactors)
+        x = x // F[x]
     primeFactors += [x]
     return primeFactors
-
-print(sieve(17))
-print(arrayF(20))
-print(factorization(19, arrayF(20)))
--- CountSemiprimes/test/test_countSemiprimes.py
+++ CountSemiprimes/test/test_countSemiprimes.py
@@ -1,17 +1,17 @@
 import pytest
-from src.countSemiprimes import solution, arrayF, sieve, factorization, getRange, sieveNums, getSemiprimes
+from src.countSemiprimes import solution, arrayF, sieve, factorization
 
 def test_case_sieve():
     assert sieve(17) == [False, False, True, True, False, True, False, True, False, False, False, True, False, True, False, False, False, True]
 
-def test_case_sieve_nums():
-    assert sieveNums(17) == [2, 3, 5, 7, 11, 13, 17]
+# def test_case_sieve_nums():
+#     assert sieveNums(17) == [2, 3, 5, 7, 11, 13, 17]
 
-def test_get_range():
-    assert getRange([1, 4, 16], [26, 10, 20]) == [(1, 26), (4, 10), (16, 20)]
+# def test_get_range():
+#     assert getRange([1, 4, 16], [26, 10, 20]) == [(1, 26), (4, 10), (16, 20)]
 
-def test_getSemiprimes():
-    assert getSemiprimes(26, [1, 4, 16], [26, 10, 20]) == [4, 6, 9, 10, 14, 15, 21, 22, 25, 26]
+# def test_getSemiprimes():
+#     assert getSemiprimes(26, [1, 4, 16], [26, 10, 20]) == [4, 6, 9, 10, 14, 15, 21, 22, 25, 26]
 
 def test_abs():
     assert round(26 ** 0.5) == 5
@@ -34,4 +34,5 @@ def test_case_arrayF():
     assert arrayF(20) == [0, 0, 0, 0, 2, 0, 2, 0, 2, 3, 2, 0, 2, 0, 2, 3, 2, 0, 2, 0, 2]
 
 def test_case_factorization():
-    assert factorization(19, arrayF(20)) == [19]
+    a = arrayF(20)
+    assert factorization(20, a) == [2, 2, 5]
