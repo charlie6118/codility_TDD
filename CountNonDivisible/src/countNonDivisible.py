a5e6b4a4420837b09661cfdcdd4c348d7f53380b
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-04-15 15:48:33 +0800

    CountNonDivisible -Credit MartinKysel.com

--- CountNonDivisible/src/countNonDivisible.py
+++ CountNonDivisible/src/countNonDivisible.py
@@ -1,2 +1,31 @@
 def solution(A):
-    pass
+  
+    maxA = max(A)
+    count = {} # count the num of each element
+    divisors = {} # decouple the divisors
+    for e in A:
+        if not count.get(e):
+            count[e] = 1
+        else:
+            count[e] += 1
+        divisors[e] = set([1, e])
+    
+    d = 2
+    while d * d <= maxA:
+        e_candidate = d
+        while e_candidate <= maxA:
+            if e_candidate in divisors and not d in divisors[e_candidate]:
+                divisors[e_candidate].add(d)
+                divisors[e_candidate].add(e_candidate // d)
+            e_candidate += d
+        d += 1
+
+    result = [0] * len(A)
+    for index, value in enumerate(A):
+        temp_sum = 0
+        for d in divisors[value]:
+            # if count has no d, return zero
+            temp_sum += count.get(d, 0)
+        result[index] = len(A) - temp_sum
+        
+    return result
--- CountNonDivisible/test/test_countNonDivisible.py
+++ CountNonDivisible/test/test_countNonDivisible.py
@@ -1,2 +1,13 @@
 import pytest
 from src.countNonDivisible import solution
+
+def test_case1():
+    assert solution([3, 1, 2, 3, 6]) == [2, 4, 3, 2, 0]
+
+def test_dictionary():
+    dic = {}
+    dic[3] = 4
+    dic[6] = 0
+    print(dic)
+    assert dic.get(3) == 4
+    assert not dic.get(5)
