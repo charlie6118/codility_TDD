f590ee34e01c0ec3d29d7815e5ebc5f61c196c7c
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-04-24 19:35:40 +0800

    two more test case

--- CountWeeksBetweenMonths/test/test_countWeeksBetweenMonths.py
+++ CountWeeksBetweenMonths/test/test_countWeeksBetweenMonths.py
@@ -6,3 +6,9 @@ def test_case1():
 
 def test_case2():
     assert solution(2019, 2, 4, 2) == 14
+
+def test_case3():
+    assert solution(2018, 4, 10, 1) == 31
+
+def test_case4():
+    assert solution(2018, 4, 7, 1) == 18
