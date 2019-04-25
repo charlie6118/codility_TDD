10240dec439c8ee4c294e06e1aefe41c7b3cf0c7
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-04-25 14:06:46 +0800

    refactor binaryGap

--- BinaryGap/src/binaryGap.py
+++ BinaryGap/src/binaryGap.py
@@ -1,25 +1,11 @@
 def solution(N):
     binaryStr = '{:b}'.format(N)
     count = 0
-    zeroList = binaryStr.split('1')
-    if binaryStr[-1:] == '0':
-        zeroList.pop()
+    zeroLists = binaryStr.split('1')
+    if binaryStr[-1] == '0':
+        zeroLists.pop()
 
-    for zeroSet in zeroList:
-        if len(zeroSet) > count:
-            count = len(zeroSet)
+    for zeroList in zeroLists:
+        if len(zeroList) > count:
+            count = len(zeroList)
     return count
-
-    # count = 0
-    # buffer = 0
-    # findOne = False
-    # for e in binaryStr:
-    #     if e == '1' and findOne == False:
-    #         findOne = True
-    #     elif e == '1' and findOne == True:
-    #         findOne = False
-    #         if count < buffer:
-    #             count = buffer
-    #     if e == '0' and findOne == True:
-    #         buffer += 1
-    # return count
