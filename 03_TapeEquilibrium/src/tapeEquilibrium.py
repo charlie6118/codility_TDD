47772bfd4049fe88242eeb5ec36533900022505d
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-05-02 14:09:07 +0800

    Tape Equilibrium -loop range modify according to requirement

--- 03_TapeEquilibrium/src/tapeEquilibrium.py
+++ 03_TapeEquilibrium/src/tapeEquilibrium.py
@@ -1,17 +1,9 @@
-import array as arr
-
 def solution(A):
-    head = A[0]
-    tail = sum(A[1:])
-    minDif = abs(head - tail)
-    if len(A) == 2:
-        return minDif
-
-    for index, data in enumerate(A):
-        if index != 0:
-            head += data
-            tail -= data
-            minDif = min(minDif, abs(head - tail))
-    return minDif
-
-print(solution(arr.array('h', [3, 1, 2, 4, 3])))
+    right = sum(A[1:])
+    left = A[0]
+    min_v = abs(right - left)
+    for i in range(1, len(A) - 1):
+        left += A[i]
+        right -= A[i]
+        min_v = min(min_v, abs(right - left))
+    return min_v
--- 03_TapeEquilibrium/test/test_tapeEquilibrium.py
+++ 03_TapeEquilibrium/test/test_tapeEquilibrium.py
@@ -1,9 +1,11 @@
 import pytest
-import array as arr
 from src.tapeEquilibrium import solution
 
 def test_sample1():
-    assert solution(arr.array('b', [3, 1, 2, 4, 3])) == 1
+    assert solution([3, 1, 2, 4, 3]) == 1
 
 def test_twoElement():
-    assert solution(arr.array('h', [-1000, 1000])) == 2000
+    assert solution([-1000, 1000]) == 2000
+
+def test_sample2():
+    assert solution([-10, -20, -30, -40, 100]) == 20
