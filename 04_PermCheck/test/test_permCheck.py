1851542cc1e35acdb29da27a7fc533de1ed7a85a
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-05-02 14:26:59 +0800

    refactor permcheck

--- 04_PermCheck/test/test_permCheck.py
+++ 04_PermCheck/test/test_permCheck.py
@@ -1,12 +1,11 @@
 import pytest
-import array as arr
 from src.permCheck import solution
 
 def test_simpleTrue():
-    assert solution(arr.array('b', [4, 1, 3, 2])) == 1
+    assert solution([4, 1, 3, 2]) == 1
 
 def test_simpleFalse():
-    assert solution(arr.array('b', [4, 1, 3])) == 0
+    assert solution([4, 1, 3]) == 0
 
 def test_totalSumCorrectButNotPermutation():
-    assert solution(arr.array('b', [3, 0, 3])) == 0
+    assert solution([3, 0, 3]) == 0
